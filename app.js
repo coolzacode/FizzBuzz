dcc5581037041afd0f46b8710d88a321a0574c7c
hazzaah <154761475+hazzaah@users.noreply.github.com>
2026-05-16 08:06:54 -0500

    refactor: simplify analyzeNumber logic and apply input validation

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-function analyzeNumber (num) {
-    const divByThree = (num % 3 === 0) ? "Fizz" : "";
-    const divByFive = (num % 5 === 0) ? "Buzz" : "";
+function analyzeNumber(rawInput) {
+    const num = Number(rawInput);
 
-    return (divByThree + divByFive) ? (divByThree + divByFive) : num;
+    if (Number.isNaN(num)) {
+        return "invalid input"
+    }
+
+    const divByThree = (num % 3 === 0);
+    const divByFive = (num % 5 === 0);
+    let result = "";
+
+    if (divByThree) result += "Fizz"; 
+    if (divByFive) result += "Buzz";
+
+    return result || `${num}`
 }
 
-console.log(analyzeNumber(3));
-console.log(analyzeNumber(5));
-console.log(analyzeNumber(7));
-console.log(analyzeNumber(15));
+
+console.log(analyzeNumber(3)); // Fizz
+console.log(analyzeNumber(5)); // Buzz
+console.log(analyzeNumber(7)); // 7
+console.log(analyzeNumber(15)); // FizzBuzz
+console.log(analyzeNumber("hi")); // invalid input
